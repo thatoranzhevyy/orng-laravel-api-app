5d2b6ced61d53dfe6ff18a229405b3820b196b9d
thatoranzhevyy <thatoranzhevyy@gmail.com>
2024-07-21 13:53:40 +0500

    update

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    $artwork = \App\Models\Asset::inRandomOrder()->get();
+    $artwork = \App\Models\Asset::inRandomOrder()->first();
     return view('welcome', compact('artwork'));
 });
